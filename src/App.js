325974f531b9babe7bb89514376555b857e41648
Aleksandra Chrapkowska <amchrapkowska@gmail.com>
2019-09-17 14:13:35 +0200

    async component splitting

--- src/App.js
+++ src/App.js
@@ -2,41 +2,53 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Page1 from './components/Page1'
+import AsyncComponent from './components/AsyncComponent'
 
 class App extends Component {
   constructor(){
     super()
     this.state = {
       route: 'Page1',
-      component: ''
+      component: null
     }
 
   }
 
   onRouteChange = route => {
-
-    if(route==='Page1')
     this.setState({route: route})
-    else if(route==='Page2'){
-      import('./components/Page2')
-      .then(Page2 => {
-        this.setState({route: route, component: Page2.default})
-      })
-    }
-    else if(route==='Page3'){
-      import('./components/Page3')
-      .then(Page3 => {
-        this.setState({route: route, component: Page3.default})
-      })
-    }
+    // if(route==='Page1')
+    // this.setState({route: route})
+    // else if(route==='Page2'){
+    //   import('./components/Page2')
+    //   .then(Page2 => {
+    //     this.setState({route: route, component: Page2.default})
+    //   })
+    // }
+    // else if(route==='Page3'){
+    //   import('./components/Page3')
+    //   .then(Page3 => {
+    //     this.setState({route: route, component: Page3.default})
+    //   })
+    // }
   }
 
   render(){
 
-    if(this.state.route === 'Page1')
+    if(this.state.route==='Page1')
     return <Page1 onRouteChange={this.onRouteChange}/>
-    else
-    return <this.state.component onRouteChange={this.onRouteChange}/>
+    else if(this.state.route==='Page2'){
+      const AsyncPage2 = AsyncComponent(()=>import('./components/Page2'))
+      return <AsyncPage2 onRouteChange={this.onRouteChange}/>
+    }
+    else if(this.state.route==='Page3'){
+      const AsyncPage3 = AsyncComponent(()=>import('./components/Page3'))
+      return <AsyncPage3 onRouteChange={this.onRouteChange}/>
+    }
+
+    // if(this.state.route === 'Page1')
+    // return <Page1 onRouteChange={this.onRouteChange}/>
+    // else
+    // return <this.state.component onRouteChange={this.onRouteChange}/>
 
   };
 }
