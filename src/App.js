b995c33e843ee4698d32e1f0454cd89069c5df7f
Aleksandra Chrapkowska <amchrapkowska@gmail.com>
2019-09-17 13:18:02 +0200

    before splitting code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Page1 from './components/Page1'
+import Page2 from './components/Page2'
+import Page3 from './components/Page3'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      route: 'Page1',
+    }
+
+  }
+
+  onRouteChange = route => {
+    // More descriptive way, second route may be ommitted tho
+    this.setState({route: route})
+  }
+
+  render(){
+    if(this.state.route === 'Page1')
+    return <Page1 onRouteChange={this.onRouteChange}/>
+    else if(this.state.route === 'Page2')
+    return <Page2 onRouteChange={this.onRouteChange}/>
+    else if(this.state.route === 'Page3')
+    return <Page3 onRouteChange={this.onRouteChange}/>
+  };
 }
 
 export default App;
