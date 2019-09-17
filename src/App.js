6556cb25a16926537f239c1e6e3a8eb2ac7c5ed9
Aleksandra Chrapkowska <amchrapkowska@gmail.com>
2019-09-17 13:40:27 +0200

    code splitting v1

--- src/App.js
+++ src/App.js
@@ -2,30 +2,42 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Page1 from './components/Page1'
-import Page2 from './components/Page2'
-import Page3 from './components/Page3'
 
 class App extends Component {
   constructor(){
     super()
     this.state = {
       route: 'Page1',
+      component: ''
     }
 
   }
 
   onRouteChange = route => {
-    // More descriptive way, second route may be ommitted tho
+
+    if(route==='Page1')
     this.setState({route: route})
+    else if(route==='Page2'){
+      import('./components/Page2')
+      .then(Page2 => {
+        this.setState({route: route, component: Page2.default})
+      })
+    }
+    else if(route==='Page3'){
+      import('./components/Page3')
+      .then(Page3 => {
+        this.setState({route: route, component: Page3.default})
+      })
+    }
   }
 
   render(){
+
     if(this.state.route === 'Page1')
     return <Page1 onRouteChange={this.onRouteChange}/>
-    else if(this.state.route === 'Page2')
-    return <Page2 onRouteChange={this.onRouteChange}/>
-    else if(this.state.route === 'Page3')
-    return <Page3 onRouteChange={this.onRouteChange}/>
+    else
+    return <this.state.component onRouteChange={this.onRouteChange}/>
+
   };
 }
 
